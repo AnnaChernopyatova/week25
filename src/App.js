ed9ff4bf3afc4db3601803fd8788e240593c5bc3
AnnaChernopyatova <anna.chernopyatova913@mail.ru>
2021-08-11 20:08:00 +0300

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Pie } from 'react-chartjs-2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const data = {
+  labels: ['Blue', 'Pink', 'Yellow', 'Purple', 'Red'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [10, 20, 30, 15, 25],
+      backgroundColor:[
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+      ],
+      borderColor: [
+        'rgba(54, 162, 235, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 99, 132, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const PieChart =() => (
+  < div className='pieChart'>
+    <h1 className='pieChartTitle'>My First Pie Chart</h1>
+    <Pie data={data} />
+  </div>
+)
+
+
+
+
+export default PieChart;
